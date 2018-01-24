134365640b67a89fb37ebd7d7c492f85ba2156af
Alex <phelangood72@gmail.com>
2018-01-24 09:14:34 -0600

    Updated with some functionality

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,12 +2,19 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+	result = 0
+	if !arr.empty?
+		arr.each {|x| result=result+x}
+	end
+	return result
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	result = 0
+	if !arr.empty?
+	end
+	return result
 end
 
 def sum_to_n? arr, n
@@ -17,15 +24,33 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+	result = "Hello, " + name
+  	return result
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	if s.empty?
+		puts 1
+		return false
+	elsif s[0].downcase.match(/[aeiou]/)
+		puts 2
+		return false
+	elsif s[0].match(/[[:alpha]]/)
+		puts 3
+		return true
+	end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	if s.empty?
+		return false
+	elsif s.match(/[A-Za-z]/)
+		return false
+	elsif s.to_i(2) % 4 == 0
+	 	return true
+	else
+	 	return false
+	end
 end
 
 # Part 3
